55dd7da2afa1e16edb090bd14896d473246245b1
mahi <127742360+mahi1974@users.noreply.github.com>
2024-12-07 10:45:56 +0530

    Create 01_Variable.js

---
+++ 01-Basics/01_Variable.js
@@ -0,0 +1,12 @@
+const accountId=121
+let accountEmail="asda@gmai.com"
+var acccountPass="21312ms"
+accountCity="veraval"
+
+let accountState;
+console.log(accountId);
+/*
+Prefer not use var
+because of issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,acccountPass,accountCity,accountState])
